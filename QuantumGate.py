63a78c55433561df893360b8372418629d5c2bbf
JD <a920423588@gmail.com>
2021-02-04 15:28:54 +0000

    Finish Hardmard Gate and Tensorproduct

---
+++ QuantumGate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import math
+
+I = np.eye(2)
+
+PX = np.array([[0, 1], 
+               [1, 0]])
+
+PY = np.array([[0, -np.complex(0, 1)], 
+               [np.complex(0, 1), 0]])
+
+PZ = np.array([[1, 0], 
+               [0, -1]])
+
+H = np.array([[1/np.sqrt(2), 1/np.sqrt(2)], 
+              [1/np.sqrt(2), -1/np.sqrt(2)]])
+
+SWAP = np.array([[1, 0, 0, 0], 
+                [0, 0, 1, 0],
+                [0, 1, 0, 0],
+                [0, 0, 0, 1]])
+
+CN = np.array([[1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 0, 1],
+                [0, 0, 1, 0]])
+
