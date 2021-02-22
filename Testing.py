396a113a4eac2dcc6cb49ccfa974ca1803ee2922
atc16 <77048564+atc16@users.noreply.github.com>
2021-02-22 10:55:11 +0000

    updated testing class

--- Testing.py
+++ Testing.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+<<<<<<< HEAD
+# In[244]:
+
+
+import qiskit
+from qiskit import Aer
+import numpy as np 
+import QuantumCircuit 
+class Testing:
+    def __init__(self, part,test_no):
+        self.part = str(part)
+        self.test_no = test_no
+=======
 # In[1]:
 
 
@@ -10,6 +23,7 @@ import QuantumCircuit
 class Testing:
     def __init__(self, part):
         self.part = str(part)
+>>>>>>> 268241f59953170bdfb7d576e069b6ad3211c2d3
     def test(self):
         return exec('self.'+self.part+'()')
     def tensor_product(self):
@@ -31,10 +45,49 @@ class Testing:
             print('Success')
         else:
             print('Fail')
+<<<<<<< HEAD
+    def get_initial_state(self):
+        testing = []
+        for qubit_no in range(1,self.test_no):
+            circ = qiskit.QuantumCircuit(qubit_no)
+            job = qiskit.execute(circ, Aer.get_backend('statevector_simulator'))
+            initial_state_correct = np.abs(job.result().get_statevector(circ, decimals = 3))
+            initial_state_to_test = QuantumCircuit.QuantumCircuit(qubit_no).get_initial_state()
+            all_zeros = not np.any(initial_state_correct - np.reshape(initial_state_to_test,(1,2**qubit_no)))
+            if all_zeros == True:
+                testing.append(0)
+            else:
+                testing.append(1)
+        if sum(testing) == 0:
+            print('Success')
+        else:
+            print('Fail')
+    def apply_hadamard(self):
+        testing = []
+        for qubit_no in range(1, self.test_no):
+            for wire_index in range(0,qubit_no ):
+                circ = qiskit.QuantumCircuit(qubit_no)
+                circ.h(wire_index)
+                job = qiskit.execute(circ, Aer.get_backend('statevector_simulator'))
+                hadamard_correct = np.abs(job.result().get_statevector(circ, decimals = 3))
+                hadamard_to_test = QuantumCircuit.QuantumCircuit(qubit_no)
+                hadamard_to_test.apply_hardmard(wire_index)
+                print(qubit_no,wire_index,hadamard_correct,np.reshape(hadamard_to_test.state,(1,2**qubit_no)))
+                all_zeros = not np.any(np.round(hadamard_correct - np.reshape(hadamard_to_test.state,(1,2**qubit_no))))
+                if all_zeros == True:
+                    testing.append(0)
+                else:
+                    testing.append(1)
+        if sum(testing) == 0:
+            print('Success')
+        else:
+            print('Fail')
+=======
 
 
 # In[ ]:
 
 
 
+>>>>>>> 268241f59953170bdfb7d576e069b6ad3211c2d3
 
