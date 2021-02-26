c104f64d5824de71f049e23e85c07cf623355554
WillHJanes <78607448+WillHJanes@users.noreply.github.com>
2021-02-26 14:35:33 +0000

    Cleaned up & added dot product
    Moved random matrix generator to a separate method for more concise code and added the dot product test method based on the tensor product test method

--- Testing.py
+++ Testing.py
@@ -10,9 +10,10 @@ import QuantumCircuit
 class Testing:
     def __init__(self, part):
         self.part = str(part)
+        #self.seed = np.random.seed()
     def test(self):
         return exec('self.'+self.part+'()')
-    def tensor_product(self):
+    def get_random_matrices(self):
         n = np.random.randint(1,10)
         W = []
         V = []
@@ -24,6 +25,9 @@ class Testing:
                 W_row.append([np.random.randint(0, 50)])
             V.append(V_row)
             W.append(W_row)
+        return V, W
+    def tensor_product(self):
+        V, W = get_random_matrices()
         tensor_product_test = qiskit.aqua.utils.tensorproduct(np.array(V),np.array(W))
         tensor_product_func = QuantumCircuit.QuantumCircuit(2).tensor_product(np.array(V),np.array(W))
         all_zeros = not np.any(tensor_product_func.reshape(n**2,n**2)-tensor_product_test.reshape(n**2,n**2))
@@ -31,10 +35,18 @@ class Testing:
             print('Success')
         else:
             print('Fail')
-
-
+    def dot_product(self):
+        V, W = get_random_matrices()
+        dot_product_test = np.matmul(np.array(V), np.array(W))
+        dot_product_func = QuantumCircuit.QuantumCircuit(2).dot_product(np.array(V), np.array(W))
+        all_zeros = not np.any(np.array([dot_product_func-dot_product_test]))
+        if (all_zeros == True):
+            print('Success')
+        else:
+            print('Fail')
+            
 # In[ ]:
 
-
+    
 
 
