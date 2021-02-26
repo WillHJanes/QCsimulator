8cd3e5a035153dc57af02221135f4f7a9e320716
Rokas Giedraitis <rokasgied@gmail.com>
2021-02-26 12:12:09 +0200

    Implemented SparseMatrix dot product

--- SparseMatrix.py
+++ SparseMatrix.py
@@ -31,10 +31,7 @@ class SparseMatrix:
         return SparseMatrix(values, rows, cols)
 
     def tensordot(self, matrix: SparseMatrix) -> SparseMatrix:
-        self_numpy = self.numpy()
-        other_numpy = matrix.numpy()
-        tensor_product = np.kron(self_numpy, other_numpy)
-        return SparseMatrix.sparsify(tensor_product)
+        return SparseMatrix.sparsify(np.kron(self.numpy(), matrix.numpy()))
 
     def numpy(self) -> np.array:
         numpy_matrix = np.zeros((self.rows, self.cols))
@@ -46,15 +43,57 @@ class SparseMatrix:
         return numpy_matrix
 
     def dot(self, matrix: SparseMatrix) -> SparseMatrix:
-        return SparseMatrix.sparsify(np.dot(self.numpy(), matrix.numpy()))
+        assert self.cols == matrix.rows, \
+            "Matrices dimensions do not match, {} != {}".format(self.cols, self.rows)
+
+        values = []
+        for i, row in enumerate(self.get_nonzero_rows()):
+            row_vals = self.get_row(int(row))
+            for j, col in enumerate(matrix.get_nonzero_cols()):
+                col_vals = matrix.get_col(int(col))
+                val = 0
+                for c in row_vals.keys():
+                    if c in col_vals.keys():
+                        val += row_vals[c] * col_vals[c]
+                values.append((int(row), int(col), val))
+
+        return SparseMatrix(values, self.rows, matrix.cols)
+
+    def get_row(self, r: int):
+        # Dictionary of values where values[col] returns value at row r and column col
+        vals = {}
+        for i in range(self.inner_array.shape[1]):
+            # The inner array is sorted by row and column. This reduces the number of iterations
+            if r < self.inner_array[0][i]:
+                break
+            elif self.inner_array[0][i] == r:
+                vals[self.inner_array[1][i]] = self.inner_array[2][i]
+        return vals
+
+    def get_col(self, c: int):
+        # Dictionary of values where values[r] returns value at row r and column c
+        vals = {}
+        for i in range(self.inner_array.shape[1]):
+            if self.inner_array[1][i] == c:
+                vals[self.inner_array[0][i]] = self.inner_array[2][i]
+        return vals
 
     def get_value(self, r: int, c: int):
-        cols = self.inner_array.shape[1]
-        for i in range(cols):
-            if r == self.inner_array[0][i]:
+        for i in range(self.inner_array.shape[1]):
+            # The inner array is sorted by row and column. This reduces the number of iterations
+            if r < self.inner_array[0][i]:
+                break
+
+            elif r == self.inner_array[0][i]:
                 if c == self.inner_array[1][i]:
                     return self.inner_array[2][i]
         return 0
+
+    def get_nonzero_rows(self) -> List:
+        return np.unique(self.inner_array[0]).tolist()
+
+    def get_nonzero_cols(self) -> List:
+        return np.unique(self.inner_array[1]).tolist()
 
 
 m1 = np.random.randint(0, 2, (3, 3))
@@ -63,6 +102,11 @@ assert np.all(m1 == s1.numpy())
 
 m2 = np.random.randint(0, 2, (3, 3))
 s2 = SparseMatrix.sparsify(m2)
+
+numpy_dot = m1.dot(m2)
+sparse_dot = s1.dot(s2)
+assert np.all(numpy_dot == sparse_dot.numpy())
+
 assert np.all(np.kron(s2.numpy(), s1.numpy()) == s2.tensordot(s1).numpy())
 
 dim = 4
