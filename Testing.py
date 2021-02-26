127318db3dc6eeb261ad2ce9e8bea353b4888297
WillHJanes <WillHJanes@users.noreply.github.com>
2021-02-26 16:01:36 +0000

    Brought into line with the Jupyter version

--- Testing.py
+++ Testing.py
@@ -1,52 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import qiskit
 import numpy as np 
 import QuantumCircuit 
-class Testing:
+class Tensor_Dot_Testing:
     def __init__(self, part):
         self.part = str(part)
         #self.seed = np.random.seed()
     def test(self):
         return exec('self.'+self.part+'()')
     def get_random_matrices(self):
-        n = np.random.randint(1,10)
-        W = []
-        V = []
-        for i in range(n):
-            V_row = []
-            W_row = []
-            for j in range(n):
-                V_row.append([np.random.randint(0, 50)])
-                W_row.append([np.random.randint(0, 50)])
-            V.append(V_row)
-            W.append(W_row)
+        W = np.random.randint(1,50,(np.random.randint(1,10),np.random.randint(1,10)))
+        V = np.random.randint(1,50,np.shape(W))
         return V, W
     def tensor_product(self):
-        V, W = get_random_matrices()
+        V, W = self.get_random_matrices()
         tensor_product_test = qiskit.aqua.utils.tensorproduct(np.array(V),np.array(W))
         tensor_product_func = QuantumCircuit.QuantumCircuit(2).tensor_product(np.array(V),np.array(W))
-        all_zeros = not np.any(tensor_product_func.reshape(n**2,n**2)-tensor_product_test.reshape(n**2,n**2))
-        if (all_zeros == True):
-            print('Success')
-        else:
-            print('Fail')
+        assert not np.any(tensor_product_func-tensor_product_test) == True, "The tensor products do not match"
     def dot_product(self):
-        V, W = get_random_matrices()
+        V, W = self.get_random_matrices()
         dot_product_test = np.matmul(np.array(V), np.array(W))
         dot_product_func = QuantumCircuit.QuantumCircuit(2).dot_product(np.array(V), np.array(W))
         all_zeros = not np.any(np.array([dot_product_func-dot_product_test]))
-        if (all_zeros == True):
-            print('Success')
-        else:
-            print('Fail')
-            
-# In[ ]:
-
+        assert not np.any(dot_product_func-dot_product_test) == True, "The dot products do not match"
     
 
 
