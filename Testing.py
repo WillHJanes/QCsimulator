e8b011b5b1bd617f295cac357a648c6a1d61bdb0
WillHJanes <willhjanes@gmail.com>
2021-03-15 13:14:20 +0000

    Better asserts and code to run method

--- Testing.py
+++ Testing.py
@@ -1,30 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import qiskit
 import numpy as np 
 import QuantumCircuit
 import sparse_matrix
 import scipy.sparse
 
-class Tensor_Dot_Testing:
+
+# In[10]:
+
+
+class Tensor_Testing:
     def __init__(self, seed=None):
         self.seed = np.random.seed(seed)
         
     def get_random_matrices(self):
-        W = np.random.randint(1,50,(np.random.randint(1,10),np.random.randint(1,10)))
-        V = np.random.randint(1,50,np.shape(W))
+        W = np.random.randint(1,50,(np.random.randint(2,10),np.random.randint(2,10)))
+        V = np.random.randint(1,50,np.shape(np.transpose(W)))
         return V, W
     
-    def tensor_product_test(self):
-        V, W = self.get_random_matrices()
+    def tensor_product_test(self, V=None, W=None):
+        if V==None or W==None:
+            V, W = self.get_random_matrices()
         tensor_product_test = qiskit.aqua.utils.tensorproduct(np.array(V),np.array(W))
         tensor_product_func = QuantumCircuit.QuantumCircuit(2).tensor_product(np.array(V),np.array(W))
-        assert not np.any(tensor_product_func-tensor_product_test) == True, "The tensor products do not match"
+        assert not np.any(tensor_product_func-tensor_product_test) == True, "The tensor products do not match. {} != {}".format(tensor_product_func, tensor_product_test)
     
-    def dot_product_test(self):
-        V, W = self.get_random_matrices()
-        dot_product_test = np.matmul(np.array(V), np.array(W))
-        dot_product_func = QuantumCircuit.QuantumCircuit(2).dot_product(np.array(V), np.array(W))
-        all_zeros = not np.any(np.array([dot_product_func-dot_product_test]))
-        assert not np.any(dot_product_func-dot_product_test) == True, "The dot products do not match"
+    #def dot_product_test(self):
+    #    V, W = self.get_random_matrices()
+    #    dot_product_test = np.matmul(np.array(V), np.array(W))
+    #    dot_product_func = QuantumCircuit.QuantumCircuit(2).dot_product(np.array(V), np.array(W))
+    #    all_zeros = not np.any(np.array([dot_product_func-dot_product_test]))
+    #    assert not np.any(dot_product_func-dot_product_test) == True, "The dot products do not match"
+
+
+# In[11]:
+
 
 class Gate_For_Test:
     '''
@@ -40,6 +55,10 @@ class Gate_For_Test:
         self.our_name = our_name
         self.num_qubits = num_qubits
 
+
+# In[21]:
+
+
 class Gate_Testing:
     '''
     Tests the given gate with both qiskit and our simulator and compares the results
@@ -50,26 +69,29 @@ class Gate_Testing:
     Pauli-Y - y
     Pauli-Z - z
     SWAP - swap
+    Controlled-Z - cz
+    Controlled-NOT - cx
     
     Parameters:
+    qubits - size of test circuits. Type should be int.
+    
     gate_input - gate to be tested. Should be a str corresponding to the qiskit name of the gate (for brevity; this can be 
                 changed easily if required).
-    qubits - size of test circuits. Type should be int.
     test_qubit_1 - first qubit to apply gate to. Will raise an error if outside the registry. Should be int.
     test_qubit_2 - second target for use with 2- and 3-qubit gates ONLY. Type should be int. For gates with a control and a 
                 target, the first test qubit is the control and the second is the target.
     test_qubit_3 - third target for use with 3-qubit gates ONLY. Type should be int.
     test_angle - angle for use with Rotation gates. Type can be int or float.
     '''
-    def __init__(self, gate_input, qubits):
-        self.gate_input = str(gate_input)
+    def __init__(self, qubits):
         self.num_qubits = qubits
         self.qiskit_circ = qiskit.QuantumCircuit(qubits)
         self.our_circ = QuantumCircuit.QuantumCircuit(qubits)
         
         self.gate_database = [Gate_For_Test("h", "apply_hardmard", 1), Gate_For_Test("x", "apply_pauliX", 1), 
                               Gate_For_Test("y", "apply_pauliY", 1), Gate_For_Test("z", "apply_pauliZ", 1), 
-                              Gate_For_Test("swap", "apply_swap", 2)]
+                              Gate_For_Test("swap", "apply_swap", 2), Gate_For_Test("cz", "apply_controlZ", 2),
+                              Gate_For_Test("cx", "apply_cnot", 2)]
     
     def run_qiskit_circuit(self,circ):
         backend = qiskit.Aer.get_backend('statevector_simulator')
@@ -88,7 +110,7 @@ class Gate_Testing:
         elif len(test_qubits) == 1:
             exec("self.qiskit_circ." + str(qiskit_gate) + "(" + str(test_qubits[0]) + ")")
         else:
-            raise ValueError("Invalid number of target qubits provided to qiskit_gate_test.")
+            raise ValueError("Invalid number of target qubits provided to qiskit_gate_test ({})".format(len(test_qubits)))
         qiskit_output = self.run_qiskit_circuit(self.qiskit_circ)
         return qiskit_output
    
@@ -100,61 +122,84 @@ class Gate_Testing:
         elif len(test_qubits) == 1:
             exec("self.our_circ." + str(our_gate) + "(" + str(test_qubits[0]) + ")")
         else:
-            raise ValueError("Invalid number of target qubits provided to our_gate_test.")
+            raise ValueError("Invalid number of target qubits provided to our_gate_test ({})".format(len(test_qubits)))
         our_output = np.transpose(self.our_circ.state)[0].astype(complex)
         return our_output
     
-    def gate_test(self, test_qubit_1, test_qubit_2=None, test_qubit_3=None, test_angle=None):
+    def gate_test(self, gate_input, test_qubit_1, test_qubit_2=None, test_qubit_3=None, test_angle=None):
+        assert np.size(np.unique(np.array([x for x in [test_qubit_1, test_qubit_2, test_qubit_3] if x != None]))) == np.size(np.array([x for x in [test_qubit_1, test_qubit_2, test_qubit_3] if x != None])), "Test qubits may not contain any duplicates. {} != {}".format(np.unique(np.array([x for x in [test_qubit_1, test_qubit_2, test_qubit_3] if x != None])), np.array([x for x in [test_qubit_1, test_qubit_2, test_qubit_3] if x != None]))
+        
         test_qubits_qiskit = [x for x in [test_qubit_1, test_qubit_2, test_qubit_3] if x != None]
         test_qubits_ours = np.subtract(np.dot(np.ones_like(test_qubits_qiskit), self.num_qubits-1), test_qubits_qiskit)
         
-        gate_this_test = next((x for x in self.gate_database if x.qiskit_name == self.gate_input), None)
+        gate_this_test = next((x for x in self.gate_database if x.qiskit_name == gate_input), None)
         
-        assert len(test_qubits_qiskit)<=gate_this_test.num_qubits, "Number of test qubits provided is greater than number given gate operates on"
-        assert np.max(np.array(test_qubits_qiskit))<self.num_qubits, "Test qubit index greater than largest register index"
+        assert len(test_qubits_qiskit)<=gate_this_test.num_qubits, "Number of test qubits provided is greater than number given gate operates on. {} > {}".format(len(test_qubits_qiskit), gate_this_test.num_qubits)
+        assert np.max(np.array(test_qubits_qiskit))<self.num_qubits, "Test qubit index greater than largest register index. {} >= {}".format(np.max(np.array(test_qubits_qiskit)), self.num_qubits)
         
         qiskit_output = self.qiskit_gate_test(gate_this_test.qiskit_name, test_qubits_qiskit, test_angle)
         our_output = self.our_gate_test(gate_this_test.our_name, test_qubits_ours)
         
-        assert np.equal(qiskit_output, our_output).all(), "The states after the gate's application do not match."
+        #tolerance implemented to deal with miniscule errors that were throwing the test off
+        assert (np.abs(np.subtract(qiskit_output, our_output)) <= 0.00005).all(), "The states after the gate's application do not match. {} != {}".format(qiskit_output, our_output)
+
+
+# In[43]:
+
 
 class Sparse_Testing:
-    def basic_sparsify_test(self, test_matrix):
+    def __init__(self, seed=None):
+        self.seed = np.random.seed(seed)
+        
+    def get_random_matrices(self):
+        test_matrix_1 = np.random.randint(1,50,(np.random.randint(1,10),np.random.randint(1,10)))
+        test_matrix_2 = np.random.randint(1,50,np.shape(np.transpose(test_matrix_1)))
+        return test_matrix_1, test_matrix_2
+    
+    def basic_sparsify_test(self, test_matrix=None):
         '''
         Ensures that the sparsify and numpy methods are consistent for the given matrix by converting to sparse and back again
         and comparing the result with the original matrix passed to the method.
         
         Parameters:
-        test_matrix - matrix to convert. Must be a numpy array.
+        test_matrix - matrix to convert. Must be a numpy array.  Will be generated randomly if not provided.
         '''
+        if test_matrix==None:
+            test_matrix = self.get_random_matrices()[0]
         original_matrix = test_matrix
         test_matrix = sparse_matrix.SparseMatrix.sparsify(test_matrix)
         test_matrix = sparse_matrix.SparseMatrix.numpy(test_matrix)
-        assert np.equal(test_matrix, original_matrix).all(), "The matrix does not match its original form."
+        assert np.equal(test_matrix, original_matrix).all(), "The matrix does not match its original form. {} != {}".format(test_matrix, original_matrix)
     
-    def sparse_dot_test(self, test_matrix_1, test_matrix_2):
+    def sparse_dot_test(self, test_matrix_1=None, test_matrix_2=None):
         '''
         Performs a dot product with both scipy.sparse and our class and confirm that the results are identical.
         
         Parameters:
-        test_matrix_1, test_matrix_2 - matrices to multiply. Must be numpy arrays.
+        test_matrix_1, test_matrix_2 - matrices to multiply. Must be numpy arrays. Will be generated randomly if not provided.
         '''
+        if test_matrix_1==None or test_matrix_2==None:
+            test_matrix_1, test_matrix_2 = self.get_random_matrices()
+            
         our_dot = sparse_matrix.SparseMatrix.numpy(sparse_matrix.SparseMatrix.sparsify(test_matrix_1).dot(sparse_matrix.SparseMatrix.sparsify(test_matrix_2)))
         scipy_dot = scipy.sparse.csc_matrix(test_matrix_1).dot(scipy.sparse.csc_matrix(test_matrix_2)).toarray()
-        assert np.equal(our_dot, scipy_dot).all(), "The output matrices do not match."
+        assert np.equal(our_dot, scipy_dot).all(), "The output matrices do not match. {} != {}".format(our_dot, scipy_dot)
         
-    def sparse_tensor_dot_test(self, test_matrix_1, test_matrix_2):
+    def sparse_tensor_dot_test(self, test_matrix_1=None, test_matrix_2=None):
         '''
         Performs a tensor dot product with both scipy.sparse and our class and confirm that the results are identical.
         
         Parameters:
-        test_matrix_1, test_matrix_2 - matrices to multiply. Must be numpy arrays.
+        test_matrix_1, test_matrix_2 - matrices to multiply. Must be numpy arrays. Will be generated randomly if not provided.
         '''
+        if test_matrix_1==None or test_matrix_2==None:
+            test_matrix_1, test_matrix_2 = self.get_random_matrices()
+            
         our_tensor_dot = sparse_matrix.SparseMatrix.numpy(sparse_matrix.SparseMatrix.sparsify(test_matrix_1).tensordot(sparse_matrix.SparseMatrix.sparsify(test_matrix_2)))
         scipy_tensor_dot = scipy.sparse.kron(scipy.sparse.csc_matrix(test_matrix_1), scipy.sparse.csc_matrix(test_matrix_2)).toarray()
-        assert np.equal(our_tensor_dot, scipy_tensor_dot).all(), "The output matrices do not match."
+        assert np.equal(our_tensor_dot, scipy_tensor_dot).all(), "The output matrices do not match.{} != {}".format(our_tensor_dot, scipy_tensor_dot)
         
-    def get_attribute_test(self, test_matrix, operation, *args):
+    def get_attribute_test(self, operation, *args, test_matrix=None):
         '''
         Performs the 'get' method corresponding to the operation given on the sparse form of test_matrix, passing the given 
         arguments, then compares the output to the original dense matrix to ensure correctness.
@@ -167,32 +212,39 @@ class Sparse_Testing:
         nonzero_cols - Gets all columns with nonzero elements
         
         Parameters:
-        test_matrix - matrix to operate on. Must be a numpy array.
+        test_matrix - matrix to operate on. Must be a numpy array. Will be generated randomly if one is not provided.
         operation - operation to perform. Must be one of those stated above or the test will return an error.
         args - arguments of the test function. Should be a single int for row and col, a pair of ints (row, col) for value, and 
         nothing for nonzero_rows and nonzero_cols.
         '''
+        if test_matrix==None:
+            test_matrix = self.get_random_matrices()[0]
+        
         output = []
         exec("output.append(sparse_matrix.SparseMatrix.sparsify(test_matrix).get_" + str(operation) + "(*args))")
         output = output[0]
         if operation == "col":
             out_keys = output.keys()
             for key in out_keys:
-                assert output[key] == test_matrix[int(key)][args[0]], "An incorrect nonzero value has been retrieved by get_col."
-            assert not np.any(np.array([test_matrix[x][args[0]] for x in range(len(test_matrix)) if x not in out_keys])), "Nonzero values in the column have not been retrived by get_col."
+                assert output[key] == test_matrix[int(key)][args[0]], "An incorrect nonzero value has been retrieved by get_col. {} != {}".format(output[key], test_matrix[int(key)][args[0]])
+            assert not np.any(np.array([test_matrix[x][args[0]] for x in range(len(test_matrix)) if x not in out_keys])), "Nonzero values in the column have not been retrived by get_col: {}.".format(np.array([test_matrix[x][args[0]] for x in range(len(test_matrix)) if x not in out_keys]))
         elif operation == "row":
             out_keys = output.keys()
             for key in out_keys:
-                assert output[key] == test_matrix[args[0]][int(key)], "An incorrect nonzero value has been retrieved by get_row."
-            assert not np.any(np.array([test_matrix[args[0]][x] for x in range(len(test_matrix[args[0]])) if x not in out_keys])), "Nonzero values in the row have not been retrived by get_row."
+                assert output[key] == test_matrix[args[0]][int(key)], "An incorrect nonzero value has been retrieved by get_row. {} != {}".format(output[key], test_matrix[args[0]][int(key)])
+            assert not np.any(np.array([test_matrix[args[0]][x] for x in range(len(test_matrix[args[0]])) if x not in out_keys])), "Nonzero values in the row have not been retrived by get_row: {}.".format(np.array([test_matrix[args[0]][x] for x in range(len(test_matrix[args[0]])) if x not in out_keys]))
         elif operation == "value":
-            assert output == test_matrix[args]
+            assert output == test_matrix[args], "The found value does not match the corresponding value in the dense matrix. {} != {}".format(output, test_matrix[args])
         elif operation == "nonzero_rows":
-            assert output == [x for x in range(len(test_matrix)) if test_matrix[x].any() == True], "Rows with nonzero elements exist that have not been retrived by get_nonzero_rows."
+            assert output == [x for x in range(len(test_matrix)) if test_matrix[x].any() == True], "Rows with nonzero elements exist that have not been retrived by get_nonzero_rows: {}.".format([x for x in range(len(test_matrix)) if test_matrix[x].any() == True and x not in output])
         elif operation == "nonzero_cols":
-            assert output == [x for x in range(len(test_matrix[0])) if np.array([test_matrix[i][x] for i in range(len(test_matrix))]).any() == True], "Columns with nonzero elements exist that have not been retrived by get_nonzero_cols."
+            assert output == [x for x in range(len(test_matrix[0])) if np.array([test_matrix[i][x] for i in range(len(test_matrix))]).any() == True],"Columns with nonzero elements exist that have not been retrived by get_nonzero_cols: {}.".format([x for x in range(len(test_matrix[0])) if np.array([test_matrix[i][x] for i in range(len(test_matrix))]).any() == True and x not in output])
         else:
-            raise ValueError("Invalid operation provided to get_attribute_test.")
+            raise ValueError("Invalid operation provided to get_attribute_test ({})".format(operation))
+
+
+# In[57]:
+
 
 class Grover_Testing:
     def __init__(self, qubits):
@@ -284,7 +336,8 @@ class Grover_Testing:
         '''
         Performs a Grover test with both simulators searching for the target state. Compares the index of the found state, and
         the value of the state itself within a given tolerance (fairly large, as the discrepency between the two simulators
-        increases significantly with the number of qubits in the circuit).
+        increases significantly with the number of qubits in the circuit). Because of differences in output format, the squares
+        of the results are compared instead of the raw results.
         
         Parameters:
         target - target state expressed as a decimal int.
@@ -292,8 +345,36 @@ class Grover_Testing:
         self.target = target
         qiskit_result = self.qiskit_grover_test()
         our_result = self.our_grover_test()
-        assert np.where(qiskit_result**2 >= complex(0.999)**2) == np.where(our_result >= 0.999), "The simulators did not find the same state."
-        assert np.abs(np.real(np.amax(qiskit_result*np.conj(qiskit_result))) - np.amax(our_result)**2) <= 0.005, "The values of the found states do not match to within +/- 0.005."
+        assert np.where(qiskit_result**2 >= complex(0.999)**2) == np.where(our_result >= 0.999), "The simulators did not find the same state. {} != {}".format(np.where(qiskit_result**2 >= complex(0.999)**2), np.where(our_result >= 0.999))
+        assert np.abs(np.real(np.amax(qiskit_result*np.conj(qiskit_result))) - np.amax(our_result)**2) <= 0.005, "The converted values of the found states do not match to within +/- 0.005. {} != {}".format(np.real(np.amax(qiskit_result*np.conj(qiskit_result))), np.amax(our_result)**2)
+
+
+# In[58]:
+
+
+Tensor_Testing().tensor_product_test()
+
+Gate_Testing(5).gate_test("h", 0)
+Gate_Testing(5).gate_test("x", 0)
+#Gate_Testing(5).gate_test("y", 0)
+Gate_Testing(5).gate_test("z", 0)
+Gate_Testing(5).gate_test("swap", 0, 1)
+Gate_Testing(5).gate_test("cz", 0, 1)
+Gate_Testing(5).gate_test("cx", 0, 1)
+
+Sparse_Testing().basic_sparsify_test()
+Sparse_Testing().sparse_dot_test()
+Sparse_Testing().sparse_tensor_dot_test()
+Sparse_Testing().get_attribute_test("col", 0)
+Sparse_Testing().get_attribute_test("row", 0)
+Sparse_Testing().get_attribute_test("value", 0, 0)
+Sparse_Testing().get_attribute_test("nonzero_rows")
+Sparse_Testing().get_attribute_test("nonzero_cols")
+
+Grover_Testing(5).grover_test(2)
+
+
+# In[ ]:
 
 
 
